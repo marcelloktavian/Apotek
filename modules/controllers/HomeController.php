032bc7cc6cac3eb90cdc0235be940aa1702340db
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2020-09-07 23:19:24 +0700

    project apotek

---
+++ modules/controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+use \modules\controllers\MainController;
+
+class HomeController extends MainController {
+
+    public function index() {
+
+
+        // $this->model('artikel'); // memanggil model artikel
+        // $data = $this->artikel->get(array(
+        //     'limit' => '0,5'
+        // )); // mendapatkan data pada model artikel
+
+        // $this->template('home', array('artikel' => $data)); // view home yang sudah dimasukkan kedalam template
+
+        $this->view('home');
+        // $this->bind('home', array('test' => $data)); 
+    }
+}
+?>
---
+++ modules/controllers/MainController.php
@@ -0,0 +1,31 @@
+<?php 
+namespace modules\controllers;
+use \Controller;
+
+class MainController extends Controller {
+
+
+	protected function template($viewName, $data = array()) {
+
+        // $this->model('artikel');
+
+        // $artikel = $this->artikel->get(
+        //     array(
+        //         'limit' => '0,5'
+        //     )
+        // );
+
+
+        // $this->model("kategori");
+
+        // $kategori = $this->kategori->get(
+        //     array(
+        //         'limit' => '0,5'
+        //     )
+        // );
+
+
+        // $view = $this->view('home');
+     
+	}
+}
